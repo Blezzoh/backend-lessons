5cb6453d19b8ea13c2429b69d0ea27eabd1fb8df
blezzoh <iradub0@sewanee.edu>
2020-04-05 16:51:22 -0700

    new file systems

---
+++ file based module/areas.js
@@ -0,0 +1,4 @@
+exports.rectange= (x, y)=>{
+    return x * y
+}
+exports.triangle = (b,h) => b * h * 1/2
---
+++ file based module/index.js
@@ -0,0 +1,7 @@
+var areas = require('./areas')
+var solveAreas = () =>{
+    console.log('triange w/ b=2, h=4', areas.triangle(2,4))
+    console.log('rectangle w/ x=2, y=4', areas.rectange(2,4))
+}
+
+solveAreas()
